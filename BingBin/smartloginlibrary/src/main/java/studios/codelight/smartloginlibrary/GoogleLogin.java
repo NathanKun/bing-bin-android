7a67ec3f54d9b0d10df995e09e8b77607b6c01c4
HE Junyang <nathanhejunyang@gmail.com>
2019-02-27 10:04:15 +0100

    remove google token

--- BingBin/smartloginlibrary/src/main/java/studios/codelight/smartloginlibrary/GoogleLogin.java
+++ BingBin/smartloginlibrary/src/main/java/studios/codelight/smartloginlibrary/GoogleLogin.java
@@ -37,7 +37,7 @@ public class GoogleLogin extends SmartLogin {
             // Configure sign-in to request the user's ID, email address, and basic
             // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
             GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .requestIdToken("***REMOVED***")
+                    .requestIdToken(TOKEN)
                     .requestEmail()
                     .requestProfile()
                     .build();
